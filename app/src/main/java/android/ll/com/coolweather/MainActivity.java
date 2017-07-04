5c92f279e065f6c21e70bd3f6f4abf6180a618db
changle <295945854@qq.com>
2017-07-04 10:21:31 +0800

    First commit.

---
+++ app/src/main/java/android/ll/com/coolweather/MainActivity.java
@@ -0,0 +1,12 @@
+package android.ll.com.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
